b07585627147774ec9fada42535715434a65e918
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-13 04:20:32 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+ 
+  def call(env)
+    resp = Rack::Response.new
+ 
+    time_1 = Kernel.int(1200)
+    time_2 = Kernel.int(2400)
+
+ 
+    resp.write "#{time_1}\n"
+    resp.write "#{time_2}\n"
+   
+ 
+    if time_1<=time_1 
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+ 
+    resp.finish
+  end
+ 
+end
