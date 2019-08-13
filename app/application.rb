e9f4078550f43d6503eba8dd05cf03694cc8ab3a
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-13 04:27:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,16 +4,16 @@ class Application
     resp = Rack::Response.new
  
     time_1 = Kernel.rand(100..1200)
-    time_2 = Kernel.rand(100..2400)
+    #time_2 = Kernel.rand(100..2400)
 
  
     resp.write "#{time_1}\n"
-    resp.write "#{time_2}\n"
+    #resp.write "#{time_2}\n"
    
  
     if time_1<=1200 
       resp.write "Good Morning!"
-    elsif time_2 >1200
+    elsif time_1 >1200
       resp.write "Good Afternoon!"
     end
  
